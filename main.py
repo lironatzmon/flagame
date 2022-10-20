c25c579f6d79dde6454e16639abc0851c7b0f743
Liron Atzmon <liron.atzmon10@gmail.com>
2022-10-20 16:32:29 +0300

    liron worked on this tuesday

--- main.py
+++ main.py
@@ -5,13 +5,39 @@ import consts
 import MineField
 
 state = {
+    "welcome statement": "Welcome to the Flag Game. \n Have Fun!",
     "is_window_open": True,
     "state": consts.RUNNING_STATE,
+    # player place is changing during the game but this is his first place
+    "player_place": (0, 0),
+    # "flag place": do we need? its consts anyway
+    "mine_places": None,
+    "grass_places": Screen.put_grass_in_field(),
+    "enter_key": True,
+    "right_button": True,
+    "left_button": True,
+    "up_button": True,
+    "down_button": True,
 }
 
 
 def main():
     pygame.init()
+    print(Screen.create())
+
+    while state["is_window_open"]:
+        handle_user_events()
+        # last row in the while#
+        Screen.draw_game(state)
+
+
+def handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+        elif state["state"] != consts.RUNNING_STATE:
+            continue
+
 
 
 if __name__ == '__main__':
