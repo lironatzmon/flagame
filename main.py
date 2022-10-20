857cbf420e8d5bfbe5e593d2babfaa13f1c4b2c4
Shiri Kriger <shiri.kriger@gmail.com>
2022-10-20 14:22:08 -0700

    main page state

---
+++ main.py
@@ -0,0 +1,18 @@
+import Screen
+import pygame
+import Soldier
+import consts
+import MineField
+
+state = {
+    "is_window_open": True,
+    "state": consts.RUNNING_STATE,
+}
+
+
+def main():
+    pygame.init()
+
+
+if __name__ == '__main__':
+    main()
