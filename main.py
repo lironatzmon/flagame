f3d3d6a66178a45661a0b6845f32220aa7eee96d
Shiri Kriger <shiri.kriger@gmail.com>
2022-10-21 17:37:00 -0700

    main

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ def main():
     #     Screen.draw_game(state)
 
 
-# def handle_user_events():
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             state["is_window_open"] = False
-#         elif state["state"] != consts.RUNNING_STATE:
-#             continue
 
+def handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+        elif state["state"] != consts.RUNNING_STATE:
+            continue
 
 
 if __name__ == '__main__':
