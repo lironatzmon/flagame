ff8e1193d07e602776a9f091a8b62f0e02f7fa4c
Liron Atzmon <liron.atzmon10@gmail.com>
2022-10-23 13:16:21 +0300

    fix something

--- Soldier.py
+++ Soldier.py
@@ -3,13 +3,12 @@ import Screen
 import consts
 
 
-def soldier(left_corner_x, left_corner_y):
+def create_soldier(left_corner_x, left_corner_y):
     soldier = pygame.image.load(consts.SOLDIER_IMAGE)
     soldier_size = pygame.transform.scale(soldier, (
         consts.SOLDIER_WIDTH, consts.SOLDIER_HEIGHT))
     Screen.screen.blit(soldier_size, (left_corner_x, left_corner_y))
     pygame.display.update()
-    return soldier, soldier_size
 
 
 def index_of_soldier(left_corner_x, left_corner_y):
