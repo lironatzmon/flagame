4ca6dedef7dae518d30157d3d029519fff60ace1
Liron Atzmon <liron.atzmon10@gmail.com>
2022-10-23 23:35:33 +0300

    fix something

--- MineField.py
+++ MineField.py
@@ -36,9 +36,9 @@ def put_mine_in_field():
             if mine_flag_grid[row_random][col_random]["content"] != "M":
                 mine_flag_grid[row_random][col_random]["content"] = "M"
                 num_mines += 1
-        mine, mine_size = Screen.draw_mine()
-        Screen.screen.blit(mine_size, (cord_x, cord_y))
-        pygame.display.update()
+        # mine, mine_size = Screen.draw_mine()
+        # Screen.screen.blit(mine_size, (cord_x, cord_y))
+        # pygame.display.update()
 
 
 def check_touch_mine(list_index_sol_legs):
@@ -47,8 +47,18 @@ def check_touch_mine(list_index_sol_legs):
             return True
 
 
-def check_touch_flag(list_index_sol_legs):
-    for row in range(23, 25):
-        for col in range(46, 50):
-            if list_index_sol_legs[1] == [row, col]:
-                return True
+# def check_touch_flag(list_index_sol_legs):
+#     for row in range(23, 25):
+#         for col in range(46, 50):
+#             if list_index_sol_legs[1] == [row, col]:
+#                 return True
+
+def check_touch_flag(list_index_sol_body):
+    if mine_flag_grid[list_index_sol_body[0][0]][list_index_sol_body[0][1]] == "F" or \
+         mine_flag_grid[list_index_sol_body[1][0]][list_index_sol_body[1][1]] == "F" or \
+            mine_flag_grid[list_index_sol_body[2][0]][list_index_sol_body[2][1]] == "F" or \
+            mine_flag_grid[list_index_sol_body[3][0]][list_index_sol_body[3][1]] == "F":
+        return True
+
+
+
