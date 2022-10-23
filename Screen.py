f1b6230dbcf9d6e8fad41bb89af7ef0266f9ac8e
Liron Atzmon <liron.atzmon10@gmail.com>
2022-10-23 10:11:31 +0300

    grass treatment

--- Screen.py
+++ Screen.py
@@ -14,7 +14,7 @@ def create():
     for i in range(consts.SQUARE_GRID_ROWS):
         row = []
         for j in range(consts.SQUARE_GRID_COLS):
-            row.append({"content": "E", "left_cord_x": j * consts.LENGTH, "left_cord_y": i * consts.LENGTH})
+            row.append("")
         field.append(row)
 
 
@@ -27,25 +27,16 @@ def draw_game(state):
 
 def put_grass_in_field(grass_img):
     for i in range(20):
-        row_random = random.randint(0, consts.SQUARE_GRID_ROWS-1)
-        col_random = random.randint(0, consts.SQUARE_GRID_COLS-1)
+        row_random = random.randint(0, consts.SQUARE_GRID_ROWS-3)
+        col_random = random.randint(0, consts.SQUARE_GRID_COLS-3)
         cord_y = int(row_random * 20)
         cord_x = int(col_random * 20)
-        field[row_random][col_random]["content"] = "G"
         grass = pygame.image.load(grass_img)
         grass_size = pygame.transform.scale(grass, (
             consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
         screen.blit(grass_size, (cord_x, cord_y))
         pygame.display.update()
 
-# def put_grass_in_field(grass_img):
-#     for i in range(20):
-#         row_random = random.randint(0, consts.WINDOW_WIDTH)
-#         col_random = random.randint(0, consts.WINDOW_HEIGHT)
-#         grass = pygame.image.load(grass_img)
-#         grass_size = pygame.transform.scale(grass, (
-#             consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
-#         screen.blit(grass_size, (row_random, col_random))
-#         pygame.display.update()
+
 
 
