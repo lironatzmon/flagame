c6ef85904444c7e60360fba86b0093fd9cb3d29e
Liron Atzmon <liron.atzmon10@gmail.com>
2022-10-23 11:02:59 +0300

    create mine field put in flag and randomly mines

--- MineField.py
+++ MineField.py
@@ -1,22 +1,41 @@
-def create():
-    global field
+import pygame
+import consts
+import random
+import Screen
+
+mine_flag_grid = []
+
+
+def create_main_matrix():
+    global mine_flag_grid
     for i in range(consts.SQUARE_GRID_ROWS):
         row = []
         for j in range(consts.SQUARE_GRID_COLS):
             row.append({"content": "E", "left_cord_x": j * consts.LENGTH, "left_cord_y": i * consts.LENGTH})
-        field.append(row)
+        mine_flag_grid.append(row)
 
 
-def put_grass_in_field(grass_img):
-    for i in range(20):
-        row_random = random.randint(0, consts.SQUARE_GRID_ROWS-1)
-        col_random = random.randint(0, consts.SQUARE_GRID_COLS-1)
+def put_flag_places():
+    for row in range(21, 24):
+        for col in range(46, 50):
+            mine_flag_grid[row][col]["content"] = "F"
+            flag, flag_size = Screen.draw_flag()
+            Screen.screen.blit(flag_size, (row * consts.LENGTH, col * consts.LENGTH))
+            pygame.display.update()
+
+
+def put_mine_in_field():
+    num_mines = 0
+    while num_mines < 20:
+        row_random = random.randint(0, consts.SQUARE_GRID_ROWS - 3)
+        col_random = random.randint(0, consts.SQUARE_GRID_COLS - 3)
         cord_y = int(row_random * 20)
         cord_x = int(col_random * 20)
-        field[row_random][col_random]["content"] = "G"
-        grass = pygame.image.load(grass_img)
-        grass_size = pygame.transform.scale(grass, (
-            consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
-        screen.blit(grass_size, (cord_x, cord_y))
+        if mine_flag_grid[row_random][col_random]["content"] == "E":
+            if mine_flag_grid[row_random][col_random]["content"] != "M":
+                mine_flag_grid[row_random][col_random]["content"] = "M"
+                num_mines += 1
+        mine, mine_size = Screen.draw_mine()
+        Screen.screen.blit(mine_size, (cord_x, cord_y))
         pygame.display.update()
 
