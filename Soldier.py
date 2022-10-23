949daedd0818f8ba452a6c6cff5218ce16a8b686
Shiri Kriger <shiri.kriger@gmail.com>
2022-10-23 16:35:30 -0700

    borders

--- Soldier.py
+++ Soldier.py
@@ -3,12 +3,39 @@ import Screen
 import consts
 
 
+# or left_corner_y > 460 or left_corner_x< 40 or left_corner_x> 960:
+
 def create_soldier(left_corner_x, left_corner_y):
     soldier = pygame.image.load(consts.SOLDIER_IMAGE)
     soldier_size = pygame.transform.scale(soldier, (
         consts.SOLDIER_WIDTH, consts.SOLDIER_HEIGHT))
-    Screen.screen.blit(soldier_size, (left_corner_x, left_corner_y))
-    pygame.display.update()
+    if left_corner_y < 0 and left_corner_x > 960:
+        Screen.screen.blit(soldier_size, (960, 0))
+        pygame.display.update()
+    elif left_corner_y > 420 and left_corner_x < 0:
+        Screen.screen.blit(soldier_size, (0, 420))
+        pygame.display.update()
+    elif left_corner_y > 420 and left_corner_x > 960:
+        Screen.screen.blit(soldier_size, (960, 420))
+        pygame.display.update()
+    elif left_corner_y < 0 and left_corner_x < 0:
+        Screen.screen.blit(soldier_size, (0, 0))
+        pygame.display.update()
+    elif left_corner_y < 0:
+        Screen.screen.blit(soldier_size, (left_corner_x, 0))
+        pygame.display.update()
+    elif left_corner_y > 420:
+        Screen.screen.blit(soldier_size, (left_corner_x, 420))
+        pygame.display.update()
+    elif left_corner_x < 0:
+        Screen.screen.blit(soldier_size, (0, left_corner_y))
+        pygame.display.update()
+    elif left_corner_x > 960:
+        Screen.screen.blit(soldier_size, (960, left_corner_y))
+        pygame.display.update()
+    else:
+        Screen.screen.blit(soldier_size, (left_corner_x, left_corner_y))
+        pygame.display.update()
 
 
 def index_of_soldier(left_corner_x, left_corner_y):
