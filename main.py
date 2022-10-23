626fd8e09a00124169c23da492636f84a0ea00ff
Liron Atzmon <liron.atzmon10@gmail.com>
2022-10-23 23:35:06 +0300

    fix something

--- main.py
+++ main.py
@@ -16,28 +16,26 @@ state = {
     "mine_places": consts.MINE_IMAGE,
     "grass_places": Screen.put_grass_in_field(),
     "key_input": False,
-    "enter_key": False,
+    # "enter_key": False,
     "is_touch_flag": True,
     "is_touch_mine": True
 }
 
-
+sol_pic = "soldier.png"
 def main():
     pygame.init()
     Screen.create()
-
+    Screen.draw_game(state)
     while state["is_window_open"]:
 
         handle_user_events()
 
         if state["key_input"]:
-            state["player_place_x"], state["player_place_y"] = move(state["player_place_x"], state["player_place_y"])
+            state["key_input"] = False
+            state["player_place_x"], state["player_place_y"] = press_check(state["player_place_x"], state["player_place_y"])
+            Screen.draw_game(state)
 
-        # if state["enter_key"]:
-        #     Screen.draw_grid()
 
-        # last row in the while
-        Screen.draw_game(state)
 
 
 def handle_user_events():
@@ -46,15 +44,13 @@ def handle_user_events():
             state["is_window_open"] = False
         elif state["state"] != consts.RUNNING_STATE:
             continue
-        elif event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             state["key_input"] = True
-        # key_input = pygame.key.get_pressed()
         # if key_input[pygame.K_KP_ENTER]:
         #     state["enter_key"] = True
 
 
-def move(cord_x, cord_y):
-    state["key_input"] = True
+def press_check(cord_x, cord_y):
     key_input = pygame.key.get_pressed()
     step = 20
     if key_input[pygame.K_LEFT]:
@@ -65,12 +61,26 @@ def move(cord_x, cord_y):
         cord_y -= step
     if key_input[pygame.K_DOWN]:
         cord_y += step
+
+    if key_input[pygame.K_KP_ENTER]:
+        Screen.draw_grid()
     return cord_x, cord_y
 
-
-# def press_enter_in_key_board():
+# def press_check(cord_x, cord_y):
 #     key_input = pygame.key.get_pressed()
+#     step = 20
+#     if key_input[pygame.K_LEFT]:
+#         cord_x -= step
+#     if key_input[pygame.K_RIGHT]:
+#         cord_x += step
+#     if key_input[pygame.K_UP]:
+#         cord_y -= step
+#     if key_input[pygame.K_DOWN]:
+#         cord_y += step
+#
 #     if key_input[pygame.K_KP_ENTER]:
+#         Screen.draw_grid()
+#     return cord_x, cord_y
 
 
 if __name__ == '__main__':
