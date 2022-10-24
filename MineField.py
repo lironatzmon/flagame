4480e0019cdd4addfcc004ddaf813c79d1a5536c
Liron Atzmon <liron.atzmon10@gmail.com>
2022-10-24 12:16:56 +0300

    fix something

--- MineField.py
+++ MineField.py
@@ -14,7 +14,9 @@ def create_main_matrix():
         for j in range(consts.SQUARE_GRID_COLS):
             row.append("E")
         mine_flag_grid.append(row)
-        return mine_flag_grid
+
+    put_flag_places()
+    put_mine_in_field()
 
 
 def put_flag_places():
@@ -27,19 +29,17 @@ def put_flag_places():
 
 
 def put_mine_in_field():
+    list_places_mines = []
     num_mines = 0
     while num_mines < 20:
         row_random = random.randint(0, consts.SQUARE_GRID_ROWS - 3)
         col_random = random.randint(0, consts.SQUARE_GRID_COLS - 3)
-        cord_y = int(row_random * 20)
-        cord_x = int(col_random * 20)
         if mine_flag_grid[row_random][col_random] == "E":
             if mine_flag_grid[row_random][col_random] != "M":
                 mine_flag_grid[row_random][col_random] = "M"
+                list_places_mines.append([row_random, col_random])
                 num_mines += 1
-        # mine, mine_size = Screen.draw_mine()
-        # Screen.screen.blit(mine_size, (cord_x, cord_y))
-        # pygame.display.update()
+    return list_places_mines
 
 
 def check_touch_mine(list_index_sol_legs):
@@ -48,16 +48,15 @@ def check_touch_mine(list_index_sol_legs):
             return True
 
 
+def check_touch_flag(list_index_sol_body):
+    if mine_flag_grid[list_index_sol_body[0][0]][list_index_sol_body[0][1]] == "F" or \
+            mine_flag_grid[list_index_sol_body[1][0]][list_index_sol_body[1][1]] == "F" or \
+            mine_flag_grid[list_index_sol_body[2][0]][list_index_sol_body[2][1]] == "F" or \
+            mine_flag_grid[list_index_sol_body[3][0]][list_index_sol_body[3][1]] == "F":
+        return True
+
 # def check_touch_flag(list_index_sol_legs):
 #     for row in range(23, 25):
 #         for col in range(46, 50):
 #             if list_index_sol_legs[1] == [row, col]:
 #                 return True
-
-def check_touch_flag(list_index_sol_body):
-    if len(mine_flag_grid) > 0:
-        if mine_flag_grid[list_index_sol_body[0][0]][list_index_sol_body[0][1]] == "F" or \
-                mine_flag_grid[list_index_sol_body[1][0]][list_index_sol_body[1][1]] == "F" or \
-                mine_flag_grid[list_index_sol_body[2][0]][list_index_sol_body[2][1]] == "F" or \
-                mine_flag_grid[list_index_sol_body[3][0]][list_index_sol_body[3][1]] == "F":
-            return True
